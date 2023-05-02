56d50416d1081b692ce97b9399d078648bd26ff3
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-05-03 02:58:28 +0600

    make chef data json file

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+app.use(cors());
+
+const chefData = require('./data/chef.json')
 
 app.get('/', (req,res) => {
     res.send('Hello world')
+})
+app.get('/chef', (req,res)=>{
+    res.send(chefData);
 })
 app.listen(port, () => {
     console.log(`my serever is running on: ${port}`)
