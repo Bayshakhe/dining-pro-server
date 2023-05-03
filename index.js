0b30a75ccd95495118d7a03e01d488ed5e0aa109
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-05-04 04:08:07 +0600

    get chef details by id

--- index.js
+++ index.js
@@ -10,9 +10,17 @@ const chefData = require('./data/chef.json')
 app.get('/', (req,res) => {
     res.send('Hello world')
 })
+
 app.get('/chef', (req,res)=>{
     res.send(chefData);
 })
+
+app.get('/chef/:id', (req,res) => {
+    const id = req.params.id;
+    const selectedChef = chefData.find(a => a.id == id);
+    res.send(selectedChef);
+})
+
 app.listen(port, () => {
     console.log(`my serever is running on: ${port}`)
 })
